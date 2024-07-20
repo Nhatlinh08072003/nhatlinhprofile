29555c96c48ae51172cb167d3336813eb2219c4f
Nhatlinh08072003 <letiennhatlinh08072003@gmail.com>
2024-07-21 00:47:52 +0700

    fafaf

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const app = express()
+const path = require('path')
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+ var duongdanfile = path.join(__dirname, 'home.html')
+ res.sendFile(duongdanfile)
+})
+
+app.listen(3000, () => {
+
+})
